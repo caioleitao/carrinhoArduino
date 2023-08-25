a57cb4d141f15fa69a849ca9df60642b09c95959
Caio Leitão <kylespace@icloud.com>
2023-08-25 13:47:49 -0300

    Update parkinson.c

--- parkinson.c
+++ parkinson.c
@@ -1,1 +1,17 @@
 //algoritmo para seguir a linha
+
+/*
+pino 2 sensor de luz
+
+Pino 10 - Dist Echo 
+Pino 8 Dist Trig
+
+pino 6 e 7 -> sentido do motor esquerdo
+pino 9 -> PWM do motor esquerdo
+
+pino 4 e 5 -> sentido do motor direito
+pino 3 -> PWM do motor direito
+*/
+
+// andar(0); -> trás
+// andar(1); -> frente
